4ad86dd4f1b5636fc316516b46faa7bdcaa4b2d5
MGZ-Prg <Mostafa_gamal199696@yahoo.com>
2020-07-20 09:05:23 +0200

    Changing the name

--- GPIO/Objects/gpio.d
+++
@@ -1,4 +0,0 @@
-./objects/gpio.o: GPIO.c GPIO.h \
-  F:\Programs\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdbool.h \
-  F:\Programs\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
-  tm4c123gh6pm.h common_macros.h
